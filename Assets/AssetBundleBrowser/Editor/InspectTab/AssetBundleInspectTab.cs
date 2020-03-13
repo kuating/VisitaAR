94c6f218467ca52e9c3d4a9002f62511ad86337c
kuating <34279732+kuating@users.noreply.github.com>
2020-03-13 16:58:08 -0300

    Final Commit

--- Assets/AssetBundleBrowser/Editor/InspectTab/AssetBundleInspectTab.cs
+++ Assets/AssetBundleBrowser/Editor/InspectTab/AssetBundleInspectTab.cs
@@ -216,6 +216,7 @@ namespace AssetBundleBrowser
                 List<AssetBundleRecord> records = new List<AssetBundleRecord>(m_loadedAssetBundles.Values);
                 foreach (AssetBundleRecord record in records)
                 {
+                    if(record != null)
                     record.bundle.Unload(true);
                 }
 
